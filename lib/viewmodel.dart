3bd112f1a315a6c88123fa9039574a1bb7116427
lucasgralcantara <lucas.alcantara@cubos.io>
2021-05-25 20:19:33 -0300

    feat: change architecture from MVC to MVVM

--- lib/viewmodel.dart
+++ lib/viewmodel.dart
@@ -1,11 +1,18 @@
+import 'dart:async';
+
 import 'package:streamexample/home_model.dart';
 
-class ControllerHome {
+class HomeViewModel {
   HomeModel _model = HomeModel();
 
   Future<List<int>> get lista => _model.lista;
 
+  StreamController<List<int>> streamLista = StreamController();
+
   void loadLista() {
     _model.loadLista();
+    _model.lista.then((value) {
+      streamLista.add(value);
+    });
   }
 }
